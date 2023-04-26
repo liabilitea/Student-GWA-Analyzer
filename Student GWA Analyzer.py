0f5736393585af19a49b44c0bdb174d1788df8e9
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 21:11:28 +0800

    Split each line into name and gwa

--- Student GWA Analyzer.py
+++ Student GWA Analyzer.py
@@ -6,8 +6,10 @@ highest_student = ""
 with open("samplerecord.txt", "r") as file:
     for line in file:    
     #Split the each line into two parts, where the first part is the students' name and the second is their GWA
-
+        name, gwa = line.strip().split(" - ")
+        gwa = float(gwa)
     # Compare the current GWA value with the highest GWA value
+    # If the current GWA is lower than highest_gwa, update highest_gwa and highest_student
     # The lower the value, the higher the GWA
 
 #Display the output
