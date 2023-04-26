9f0e2c60ff581ebb8610402fd1ac4669c3f4cc8b
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 21:08:27 +0800

    Initialize variable

--- Student GWA Analyzer.py
+++ Student GWA Analyzer.py
@@ -1,4 +1,6 @@
 #Initialize two variables as empty and none where we append the values after evaluating each line in the text
+highest_gwa = None
+highest_student = ""
 
 #Open the txt file as read mode and iterate through each line of txt with for loop
     #Split the each line into two parts, where the first part is the students' name and the second is their GWA
