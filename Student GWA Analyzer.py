f6be5485c59d5ab61e67497725feb43eeec2963d
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 21:06:00 +0800

    Pseudocode

---
+++ Student GWA Analyzer.py
@@ -0,0 +1,9 @@
+#Initialize two variables as empty and none where we append the values after evaluating each line in the text
+
+#Open the txt file as read mode and iterate through each line of txt with for loop
+    #Split the each line into two parts, where the first part is the students' name and the second is their GWA
+
+    # Compare the current GWA value with the highest GWA value
+    # The lower the value, the higher the GWA
+
+#Display the output
